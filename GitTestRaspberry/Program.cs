d4b7bf462b5df6b73258d89e17ccfb39112c84bc
Magnus Jönsson <supermoosen@hotmail.com>
2013-09-16 14:51:48 +0200

    no message

--- GitTestRaspberry/Program.cs
+++ GitTestRaspberry/Program.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GitTestRaspberry
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
